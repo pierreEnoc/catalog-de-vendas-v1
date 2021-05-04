4b3fc2d24978e83f4a5cfd8094ec22f0d8c2a17e
Pierre enoc <pierre.enoc@gympass.com>
2021-05-04 14:37:43 -0300

    entity category created

---
+++ Backend-dscatalog/src/main/java/com/pierre/dsvendas/entities/Categoria.java
@@ -0,0 +1,63 @@
+package com.pierre.dsvendas.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria)) return false;
+        Categoria categoria = (Categoria) o;
+        return getId().equals(categoria.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
