ba801be2d54ac4ecc2ae92fa691aac4f2f093c97
Pierre enoc <pierre.enoc@gympass.com>
2021-06-03 00:24:16 -0300

    :sparkles: add pagination

--- Backend-dscatalog/src/main/java/com/pierre/dsvendas/entities/services/CategoryService.java
+++ Backend-dscatalog/src/main/java/com/pierre/dsvendas/entities/services/CategoryService.java
@@ -1,20 +1,22 @@
 package com.pierre.dsvendas.entities.services;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.pierre.dsvendas.dto.CategoryDTO;
 import com.pierre.dsvendas.entities.Category;
 import com.pierre.dsvendas.entities.services.exception.DatabaseException;
 import com.pierre.dsvendas.entities.services.exception.ResourceFoundException;
 import com.pierre.dsvendas.repositories.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityNotFoundException;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -23,9 +25,9 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-      List<Category> list = categoryRepository.findAll();
-      return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+      Page<Category> list = categoryRepository.findAll(pageRequest);
+      return list.map(x -> new CategoryDTO(x));
     }
 
     @Transactional(readOnly = true)
--- Backend-dscatalog/src/main/java/com/pierre/dsvendas/resources/CategoryResource.java
+++ Backend-dscatalog/src/main/java/com/pierre/dsvendas/resources/CategoryResource.java
@@ -1,51 +1,69 @@
 package com.pierre.dsvendas.resources;
 
-import com.pierre.dsvendas.dto.CategoryDTO;
-import com.pierre.dsvendas.entities.services.CategoryService;
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
+import com.pierre.dsvendas.dto.CategoryDTO;
+import com.pierre.dsvendas.entities.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-    @Autowired
-    private CategoryService categoryService;
+	@Autowired
+	private CategoryService categoryService;
 
-    @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = categoryService.findAll();
-        return ResponseEntity.ok().body(list);
-    }
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> findByID(@PathVariable Long id) {
-        CategoryDTO dto = categoryService.findById(id);
-        return ResponseEntity.ok().body(dto);
-    }
+	) {
 
-    @PostMapping
-    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
-        dto = categoryService.insert(dto);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-                .buildAndExpand(dto.getId()).toUri();
-        return ResponseEntity.created(uri).body(dto);
-    }
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
-        dto = categoryService.update(id, dto);
-        return ResponseEntity.ok().body(dto);
-    }
+		Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
 
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
-        categoryService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findByID(@PathVariable Long id) {
+		CategoryDTO dto = categoryService.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = categoryService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		dto = categoryService.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
+		categoryService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
