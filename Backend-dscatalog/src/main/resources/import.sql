b379c799d6165aa21b4f441bb7efc009707e2b62
Pierre enoc <pierre.enoc@gympass.com>
2021-06-03 12:32:40 -0300

    :sparkles: add product entity,sql srcipt

--- Backend-dscatalog/src/main/resources/import.sql
+++
@@ -1,9 +0,0 @@
-insert INTO tb_category (name, created_At) VALUES ('Books', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Electronics', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Computers', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Books', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Electronics', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Computers', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Books', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Electronics', NOW());
-insert INTO tb_category (name, created_At) VALUES ('Computers', NOW());
