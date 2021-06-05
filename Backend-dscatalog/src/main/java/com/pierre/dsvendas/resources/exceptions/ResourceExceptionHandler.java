4a030bcbf5fc9759db4632c0d72b126787c6a415
Pierre enoc <pierre.enoc@gympass.com>
2021-06-05 00:26:30 -0300

    :sparkles: methodArgumentNotValidException

--- Backend-dscatalog/src/main/java/com/pierre/dsvendas/resources/exceptions/ResourceExceptionHandler.java
+++ Backend-dscatalog/src/main/java/com/pierre/dsvendas/resources/exceptions/ResourceExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.pierre.dsvendas.resources.exceptions;
 
-import com.pierre.dsvendas.entities.services.exception.DatabaseException;
-import com.pierre.dsvendas.entities.services.exception.ResourceFoundException;
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-import java.time.Instant;
+import com.pierre.dsvendas.entities.services.exception.DatabaseException;
+import com.pierre.dsvendas.entities.services.exception.ResourceFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -32,6 +35,18 @@ public class ResourceExceptionHandler {
         err.setTimestamp(Instant.now());
         err.setStatus(status.value());
         err.setError("Database exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandarError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        StandarError err = new StandarError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation exception");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(status).body(err);
