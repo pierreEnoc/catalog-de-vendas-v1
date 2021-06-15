38d9d822c359adec63c02c3b5da203afa125e1df
Pierre enoc <pierre.enoc@gympass.com>
2021-06-14 21:07:21 -0300

    :sparkles:test add cors

--- Backend-dscatalog/src/main/java/com/pierre/dsvendas/config/ResourceServerConfig.java
+++ Backend-dscatalog/src/main/java/com/pierre/dsvendas/config/ResourceServerConfig.java
@@ -3,7 +3,10 @@ package com.pierre.dsvendas.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +14,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -22,7 +29,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tonkenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**", "/localhost:8080/**" };
 	
 	private static final String[] OPERATRO_OR_ADMIN = { "/products/**", "/categories/**" };
 	
@@ -37,9 +44,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
-		//if (Arrays.asList( env.getActiveProfiles()).contains("test")) {
-		//	http.headers().frameOptions().disable();
-		//}
+		if (Arrays.asList( env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
@@ -47,6 +54,29 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.antMatchers(OPERATRO_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
 		.antMatchers(ADMIN).hasRole("ADMIN")
 		.anyRequest().authenticated();	
+		http.cors().configurationSource(corsConfigurationSource());
+	}
+	
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> bean 
+			= new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 	
 }
